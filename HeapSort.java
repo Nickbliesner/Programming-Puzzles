69cb5e1739fa9ab140b8f4f784b1bf1d4e7c0124
Nick Bliesner <44148050+Nickbliesner@users.noreply.github.com>
2018-12-27 18:10:17 -0800

    Added heap sort

---
+++ HeapSort.java
@@ -0,0 +1,52 @@
+package nickCodingTest;
+
+import java.util.Random;
+
+// Class that contains method to 
+// sort an int [] using a heap sort
+// (using a min heap to store and sort the given data)
+public class HeapSort {
+	public static void main(String[] args) {
+		int[] arr = new int[20];
+		Random r = new Random();
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(50) + 1;
+		}
+		
+		printArray(arr);
+		heapSort(arr);
+		printArray(arr);
+	}
+	
+	// does a heap sort on the given int array (sort using a min heap)
+	// worst case runtime complexity is O (n * log base 3 (n))
+	// as each heap operation is roughly O (log base 3 (n))
+	// as a three child heap is used
+	public static void heapSort(int[] arr) {
+		KHeap threep = new KHeap(3);
+		for (int i = 0; i < arr.length; i++) {
+			threep.add(arr[i]);
+		}
+		
+		int index  = 0;
+		while (!threep.isEmpty()) {
+			arr[index] = threep.remove(); 
+			index++;
+		}
+	}
+	
+	// prints the given array (the appropriate to string of the array)
+	public static void printArray(int[] arr) {
+		System.out.print("[");
+		if (arr.length > 0) {
+			System.out.print(arr[0]);
+		}
+		
+		for (int i = 1; i < arr.length; i++) {
+			System.out.print(", " + arr[i]);
+		}
+		
+		System.out.println("]");
+	}
+}
