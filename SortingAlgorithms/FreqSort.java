963fef8988debfbebdc3ec99f9a6faab090dd541
Nick Bliesner <44148050+Nickbliesner@users.noreply.github.com>
2019-01-01 13:44:50 -0800

    Add files via upload

---
+++ SortingAlgorithms/FreqSort.java
@@ -0,0 +1,91 @@
+package nickCodingTest;
+import nickCodingTest.MergeSort;
+import java.util.Random;
+import java.util.*;
+
+// Class that contains method to 
+// sort an int [] using a frequency sort,
+// returns a new array
+// which contains each distinct value in the original array
+// sorted by descending frequency (highest to smallest) 
+// breaking ties based on ascending order by value
+public class FreqSort {
+	public static void main(String[] args) {
+		int[] arr = new int[20];
+		Random r = new Random();
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(15) + 1;
+		}
+		
+		printArray(arr);
+		int[] mergeSorted = MergeSort.mergeSort(arr, 0, arr.length);
+		printArray(mergeSorted);
+		int[] freqSorted = freqSort(arr);
+		printArray(freqSorted);
+	}
+	
+	// does a frequency sort on the given int array
+	// and returns the sorted array of distinct values descending sorted by frequency
+	// in the original given array
+	// breaking frequency ties based on ascending by value
+	// throws an illegal argument exception if the given array is empty
+	// worst case runtime complexity is O (n * log (n)) where n is the size of the given array
+	// as a heap sort is used to sort the pairs of int and frequency
+	public static int[] freqSort(int[] arr) {
+		if (arr.length == 0) {
+			throw new IllegalArgumentException("Input array arr is length 0, no elements to sort");
+		}
+		
+		// stores counts of the different values in arr
+		Map<Integer, Integer> inv = new HashMap<Integer, Integer>();
+		
+		// compute and store the counts for all the distinct values in the
+		// given array
+		for (int i = 0; i < arr.length; i++) {
+			int val = arr[i];
+			
+			// check if there's a mapping already for this value
+			if (!inv.containsKey(val)) {
+				inv.put(val, 0);
+			}
+			
+			// update the count by 1
+			inv.put(val, inv.get(val) + 1);
+		}
+		
+		// create the min heap that will store the pairs
+		// and do the frequency sorting
+		PriorityQueue<Pair> heap = new PriorityQueue<Pair>();
+		for (int val: inv.keySet()) {
+			heap.add(new Pair(val, inv.get(val)));
+		}
+		
+		// create the array to store the values
+		int[] sorted = new int[inv.keySet().size()];
+		
+		// loop over the array taking the current minimum
+		// val out of the heap as the next element of the array
+		for (int i = 0; i < sorted.length; i++) {
+			Pair iPair = heap.remove();
+			sorted[i] = iPair.val;
+		}
+		
+		// return the new merged array
+		return sorted;
+	}
+	
+	// prints the given array (the appropriate to string of the array)
+	public static void printArray(int[] arr) {
+		System.out.print("[");
+		if (arr.length > 0) {
+			System.out.print(arr[0]);
+		}
+		
+		for (int i = 1; i < arr.length; i++) {
+			System.out.print(", " + arr[i]);
+		}
+		
+		System.out.println("]");
+	}
+}
---
+++ SortingAlgorithms/Pair.java
@@ -0,0 +1,33 @@
+package nickCodingTest;
+
+public class Pair implements Comparable<Pair> {
+	int val;
+	int freq;
+	
+	public Pair(int val, int freq) {
+		this.val = val;
+		this.freq = freq;
+	}
+	
+	// since we'll use a priority queue to do our sort by
+	// frequency we want pairs with higher frequencies to be considered
+	// less, when frequency matches well prioritize smaller values
+	public int compareTo(Pair other) {
+		// compare by frequency first
+		if (this.freq > other.freq) {
+			return -1;
+		} else if (this.freq < other.freq) {
+			return 1;
+		}
+		
+		// prioritize smaller values if they match frequency
+		if (this.val < other.val) {
+			return -1;
+		} else if (this.val > other.val) {
+			return 1;
+		}
+		
+		// the pairs are equal
+		return 0;
+	}
+}
