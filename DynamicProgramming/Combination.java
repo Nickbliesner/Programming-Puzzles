4c934a1b2de6ceaeb405e6fa33d88592a95644bc
Nick Bliesner <44148050+Nickbliesner@users.noreply.github.com>
2019-01-02 21:09:09 -0800

    Add files via upload

---
+++ DynamicProgramming/Combination.java
@@ -0,0 +1,72 @@
+package nickCodingTest;
+
+// Class that contains a method to compute
+// combinations. Given a number of elements
+// as well as a number of successes, returns how many ways
+// to pick the number of successes from that many things
+// For example: 4 C 2 = 6 -> 1 1 0 0, 1 0 1 0, 
+//							 1 0 0 1, 0 1 1 0, 
+// 							 0 1 0 1, 0 0 1 1
+public class Combination {
+
+	public static void main(String[] args) {
+		// compute the first 8 rows of pascals triangle
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.println(i + " choose " + j + " : " + getCombinations(i, j, new int[i + 1][j + 1]));
+			}
+		}
+	}
+	
+	// given an int n and an int c
+	// returns how many ways you can pick c successes out of 
+	// n total things (number of combinations n choose c)
+	// with caching worst case possible runtime is O (n ^ 2)
+	// as all elements up to the nth row of pascals triangle will need to be 
+	// computed, and the cum sum of things in pascals triangles is proportional to the row
+	// number squared
+	public static int getCombinations(int n, int c, int[][] cache) {
+		// throw an exception if n is less than 0 it doesn't make
+		// much sense to pick combinations from a negative amount of things 
+		if (n < 0) {
+			throw new IllegalArgumentException("the given int n: " + n + ", can't' be less than 0");
+		}
+		
+		// throw an exception if c is less than 0
+		// or if c is greater than n as it doesn't make sense
+		// to select a negative amount of things or more things than available
+		if (c < 0 || n < c) {
+			throw new IllegalArgumentException("the given int c: " + c + ", can't be less than 0, " + 
+											   "or greater than n: " + n);
+		}
+		
+		// base case:
+		// if we have 0 things to choose from
+		// or want all of the n things to be successes
+		// or none of the n things to be successes there's only one way to do so
+		if (n == 0 || c == 0 || c == n) {
+			return 1;
+		}
+		
+		// check whether or not this combination has been cached already
+		if (cache[n][c] != 0) {
+			return cache[n][c];
+		}
+		
+		// the combination has not been cached let's compute it
+		// we need to cover both cases: that the current element being selected
+		// is a success or it is not a success and call the combination function accordingly 
+		int sum = 0;
+		
+		// we choose the current item as a success
+		sum += getCombinations(n - 1, c - 1, cache);
+		
+		// we choose the current item as a failure
+		sum += getCombinations(n - 1, c, cache);
+		
+		// cache the calculated value
+		cache[n][c] = sum;
+		return sum;
+		
+	}
+}
---
+++ DynamicProgramming/FrogJump.java
@@ -0,0 +1,80 @@
+package nickCodingTest;
+
+// Class that contains a method,
+// given an array as the bog will return the minimum number of jumps it
+// will take a frog to hop across the bog (reach past the last index of the input array) 
+// if at a given index i the max jump the frog can take is arr[i]
+// and if at any point 0 is encountered the frog gets stuck in the bog and can't keep jumping
+public class FrogJump {
+
+	public static void main (String[] args) {
+		int[] arr = {8, 1, 1, 1, 1, 1, 9, 6, 5, 1, 1, 1, 1, 0};
+		System.out.println("the min jumps is: " + frogJump(arr));
+	}
+	
+	// given an array arr of non negative jump values,
+	// where arr[i] represents the maximum jump the frog can make from that position
+	// returns the minimum number of jumps a frog
+	// needs to make to reach the end and -1 otherwise
+	// if there's no jump sequence to cross the bog
+	public static int frogJump(int[] arr) {
+		// throw an illegal argument exception if the input
+		// array is empty, no bog means no value makes sense for jumping
+		if (arr.length == 0) {
+			throw new IllegalArgumentException(); 
+		}
+		
+		// traverse the array in reverse order. Since we always jump right
+		// as we reach each leftmost index all ones right of it will already be processed
+		// thus this can be done iteratively.
+		// instead of using a cache we can overwrite the array as we process values.
+		// Since the jump values are not needed after processing
+		for (int i = arr.length - 1; i >= 0; i--) {
+			int cur = arr[i];
+			
+			// check whether or not we can jump out from this index
+			if (arr.length <= cur + i) {
+				arr[i] = 1;
+			
+			// check if the frog can't jump from this index	
+			} else if (cur == 0) {
+				arr[i] = -1;
+				
+			// traverse all possible jumps from this index
+			// and find the min jump path the frog can take	
+			} else {
+				int min = Integer.MAX_VALUE;
+				
+				for (int j = cur; j > 0; j--) {
+					int curMin = arr[i + j];
+					
+					// check whether or not to update the
+					// min jumps from here
+					if (curMin != -1 && curMin < min) {
+						min = curMin;
+						
+						if (min == 1) {
+							// nothing can beat this minval
+							// stop searching now
+							break;
+						}
+					}
+				}
+				
+				// check whether or not we were able to find a min
+				// other than -1, if so add a jump from this index to the 
+				if (min != Integer.MAX_VALUE) {
+					min++;
+				} else {
+					min = -1;
+				}
+				
+				arr[i] = min;
+			}
+		}
+		
+		// the value at index 0 will now hold the
+		return arr[0];
+	}
+	
+}
