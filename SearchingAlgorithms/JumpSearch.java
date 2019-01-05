ad1c430150519736cc226ea8cc3e086a722e3841
Nick Bliesner <44148050+Nickbliesner@users.noreply.github.com>
2019-01-05 00:38:36 -0800

    Added linear as well as jump search

---
+++ SearchingAlgorithms/JumpSearch.java
@@ -0,0 +1,95 @@
+package nickCodingTest;
+
+import java.util.Random;
+
+import nickCodingTest.SortingAlgorithms.MergeSort;
+
+// Class that contains a method for performing 
+// a jump search on a sorted int [].
+// Employs the sorted properties of the array by
+// jumping a gap size each time through the array
+// until the elements are found to be larger than
+// the target, than a linear search is performed
+// on the gap where the target element would be 
+public class JumpSearch {
+	public static void main(String[] args) {
+		int[] arr = new int[20];
+		Random r = new Random();
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(15) + 1;
+		}
+		
+		printArray(arr);
+		int[] sorted = MergeSort.mergeSort(arr, 0, arr.length);
+		printArray(sorted);
+		int target = 11;
+		System.out.println(jumpSearch(sorted, target));
+	}
+	
+	// performs a jump search on the given  int[] arr
+	// for the target val
+	// returns boolean representing whether or not
+	// the value is present in the sorted given array
+	// Worst case runtime is O (n ^ 1/2) as if n ^ 1/2 is
+	// used as the jump gap it will minimize the number of 
+	// comparisons that need to occur when jumping
+	// as well as how many elements need to be checked when
+	// linear searching the gap where the target could be
+	// (if m is gapsize, and n is array size, comparisons is roughly-> n / m + (m - 1)
+	// the comparisons will thus be minimized when gap size = n ^ 1/2)
+	public static boolean jumpSearch(int[] arr, int target) {
+		int index  = 0;
+		int jump = (int) Math.sqrt(arr.length);
+		
+		// continue to jump through the array until we go out of
+		// bounds or the current element is larger than the target
+		while (index < arr.length && arr[index] < target) {
+			index += jump;
+		}
+		
+		// check whether or not we ended on the target
+		if (index < arr.length && arr[index] == target) {
+			return true;
+		}
+		
+		// if the target is less than the first element
+		// it can't be in the array
+		if (index == 0) {
+			return false;
+		}
+		
+		// linear search the gap range where the target could possibly be
+		int startGap = index - jump + 1;
+		for (int i = startGap; i < Math.min(arr.length, index); i++) {
+			
+			// check whether or not the value is the target
+			if (arr[i] == target) {
+				return true;
+				
+			// we passed where the target could be and since
+			// the current values larger it can't be present	
+			} else if (target < arr[i]) {
+				break;
+			}
+		}
+		
+		// the linear search was unsuccessful we couldn't find the desired target
+		return false;
+	}
+	
+	// prints the given array (the appropriate to string of the array)
+	public static void printArray(int[] arr) {
+		System.out.print("[");
+		if (arr.length > 0) {
+			System.out.print(arr[0]);
+		}
+		
+		for (int i = 1; i < arr.length; i++) {
+			System.out.print(", " + arr[i]);
+		}
+		
+		System.out.println("]");
+	}
+	
+}
---
+++ SearchingAlgorithms/LinearSearch.java
@@ -0,0 +1,58 @@
+package nickCodingTest;
+
+import java.util.Random;
+
+// Class that contains a method for performing 
+// a linear search on an int [].
+// Looks through each item one by one until
+// the target is found or looks through
+// everything and doesn't find the target
+public class LinearSearch {
+	public static void main(String[] args) {
+		int[] arr = new int[20];
+		Random r = new Random();
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = r.nextInt(15) + 1;
+		}
+		
+		printArray(arr);
+		int target = 4;
+		System.out.println(linearSearch(arr, target));
+	}
+	
+	// performs a linear search on the given  int[] arr
+	// for the target val. Looking at elements one by one
+	// to see if the target val is in the array.
+	// Returns boolean representing whether or not
+	// the value is present in the given array
+	// Worst case runtime is O (n) as the linear search
+	// will potentially have to look through everything to determine the target is missing.
+	// Alright searching algorithm for unsorted data
+	public static boolean linearSearch(int[] arr, int target) {
+		// traverse the array until the target val is found
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == target) {
+				return true;
+			}
+		}
+		
+		// if we get to this point we traversed the entire
+		// array and couldn't find the target
+		return false;
+	}
+	
+	// prints the given array (the appropriate to string of the array)
+	public static void printArray(int[] arr) {
+		System.out.print("[");
+		if (arr.length > 0) {
+			System.out.print(arr[0]);
+		}
+		
+		for (int i = 1; i < arr.length; i++) {
+			System.out.print(", " + arr[i]);
+		}
+		
+		System.out.println("]");
+	}
+}
