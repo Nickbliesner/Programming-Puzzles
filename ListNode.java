d1ec0a70df4217901d9b9b1dbcf6fd513e0427a9
Nick Bliesner <44148050+Nickbliesner@users.noreply.github.com>
2019-01-05 18:31:13 -0800

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,12 @@
+package nickCodingTest;
+
+// Data structure that represents
+// the nodes in a linked list
+public class ListNode {
+	int val;
+	ListNode next;
+	
+	public ListNode(int val) {
+		this.val = val;
+	}
+}
